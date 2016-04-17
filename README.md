2763816d89821eb8c43944755c6e821bc5b6e657
Sinan Güneş <gunes.sinan@gmail.com>
2016-04-17 21:54:23 +0300

    README added

---
+++ README.md
@@ -0,0 +1,11 @@
+# node-pvpgn-hash
+NodeJS implementation of Broken SHA-1 for PvPGN
+
+example usage:
+
+```js
+var pvpgn_hash = require('pvpgn-hash');
+
+console.log(pvpgn_hash.get('12345'));
+
+```
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var assert = require('assert');
-var pvpgn_hash = require('../pvpgn-hash')
+var pvpgn_hash = require('../pvpgn-hash');
 
 describe('PvPGN SHA1 Hash', function () {
     describe('Hash of 12345', function () {
